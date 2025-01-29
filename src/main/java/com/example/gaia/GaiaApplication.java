5ff9ebfb83b447db875727333a95225cab83923e
KangSungSu <tmdtn0312@naver.com>
2025-01-29 17:30:56 +0900

    chore: Testing deploy and build

--- src/main/java/com/example/gaia/GaiaApplication.java
+++ src/main/java/com/example/gaia/GaiaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GaiaApplication {
-
+	//Testing YAML
 	public static void main(String[] args) {
 		SpringApplication.run(GaiaApplication.class, args);
 	}
